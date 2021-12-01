345a1ed8662b350589e58f3202f6e3effc0b3c0f
Omobobola Obiwale <w0772193@myscc.ca>
2021-12-01 15:16:39 -0500

    #13 Completed Couopn Fragment - Created working coupon code with high difficulty level - Added Popup message on win

---
+++ app/src/main/java/com/example/crownpizzaapplication/CouponPackage/CouponDialog.java
@@ -0,0 +1,33 @@
+package com.example.crownpizzaapplication.CouponPackage;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+//
+public class CouponDialog extends AppCompatDialogFragment {
+//    public class CouponDialog extends FreePizza{
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Congratulations")
+                .setMessage("You just got 5 Dollars off your next Pizza!!")
+                .setPositiveButton("YAY", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+     return builder.create();
+
+
+    }
+}
